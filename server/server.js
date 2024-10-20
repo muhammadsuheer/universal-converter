661d6a19ecec197466ac77500c363130bb3c38b4
Muhammad <muhammadsuheer6@gmail.com>
2024-10-20 16:43:28 +0500

    a

--- server/server.js
+++ server/server.js
@@ -1,18 +1,1 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
-import connectDB from "./configs/mongoose.js";
-
-// App Config
-const PORT = process.env.PORT || 4000;
-
-const app = express();
-await connectDB();
-
-// Middleware Intialization
-app.use(express.json());
-app.use(cors());
-// App Routes
-app.get("/", (res, req) => res.send("API is Wrking"));
-
-app.listen(PORT, () => console.log("Server is  on Port " + PORT));
+console.log('Api is Working')
